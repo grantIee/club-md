d1ca5d0dfff9a9e4ba0c95c2698849fd7e265aec
GrantIee <grant.lee.2017@gmail.com>
2019-01-05 07:51:13 -0500

    readjusted db

--- backend/db.py
+++ backend/db.py
@@ -2,6 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Table(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key = True)
+
+    def __init__(self, **kwargs):
+        self.text = kwargs.get('text', '')
+        self.username = kwargs.get('username', '')
+        self.task_id = kwargs.get('task_id')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'score': self.score,
+            'text': self.text,
+            'username': self.username
+        }
+
 class Club(db.Model):
     __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key = True)
@@ -43,8 +60,8 @@ class User(db.Model):
             'text': self.text,
             'username': self.username
         }
-
-class Floor_Table(db.Model):
+        
+class Bid(db.Model):
     __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key = True)
     score = db.Column(db.Integer, default = 0)
