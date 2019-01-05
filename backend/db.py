28a067f3ec2d5333f2d5144387dcdf0bdd111153
yds05238 <jy392@cornell.edu>
2019-01-05 22:14:18 +0900

    Added Club and User class

--- backend/db.py
+++ backend/db.py
@@ -20,45 +20,50 @@ class Table(db.Model):
         }
 
 class Club(db.Model):
-    __tablename__ = 'comment'
+    __tablename__ = 'club'
     id = db.Column(db.Integer, primary_key = True)
-    score = db.Column(db.Integer, default = 0)
-    text = db.Column(db.String, nullable = False)
-    username = db.Column(db.String, nullable= False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable = False)
+    name = db.Column(db.String, nullable = False)
+    description = db.Column(db.String, nullable = False)
+    address = db.Column(db.String, nullable = False)
+    table_id = db.Column(db.Integer, db.ForeignKey('table.id'), nullable = False)
 
     def __init__(self, **kwargs):
-        self.text = kwargs.get('text', '')
-        self.username = kwargs.get('username', '')
-        self.task_id = kwargs.get('task_id')
+        self.name = kwargs.get('name', '')
+        self.description = kwargs.get('description', '')
+        self.address = kwargs.get('address', '')
+        self.table_id = kwargs.get('table_id')
 
     def serialize(self):
         return {
             'id': self.id,
-            'score': self.score,
-            'text': self.text,
-            'username': self.username
+            'name': self.name, 
+            'description': self.description, 
+            'address': self.address
         }
 
 class User(db.Model):
     __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key = True)
-    score = db.Column(db.Integer, default = 0)
-    text = db.Column(db.String, nullable = False)
-    username = db.Column(db.String, nullable= False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable = False)
+    username = db.Column(db.String, default = 0)
+    password = db.Column(db.String, nullable = False)
+    phone_number = db.Column(db.String, nullable= False)
+    id_number = db.Column(db.String, nullable=False)
+    bid_id = db.Column(db.Integer, db.ForeignKey('bid.id'), nullable = False)
 
     def __init__(self, **kwargs):
-        self.text = kwargs.get('text', '')
         self.username = kwargs.get('username', '')
-        self.task_id = kwargs.get('task_id')
+        self.password = kwargs.get('password', '')
+        self.phone_number = kwargs.get('phone_number', '')
+        self.id_number = kwargs.get('id_number', '')
+        self.bid_id = kwargs.get('bid_id')
 
     def serialize(self):
         return {
             'id': self.id,
-            'score': self.score,
-            'text': self.text,
-            'username': self.username
+            'username': self.username, 
+            'password': self.password, 
+            'phone_number': self.phone_number, 
+            "id_number": self.id_number
         }
         
 class Bid(db.Model):
