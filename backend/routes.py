d0e7f7a1a49106b1acc154fad0663864f44c7769
GrantIee <grant.lee.2017@gmail.com>
2019-01-12 23:56:07 -0500

    Added Backend Endpoints 1-5

--- backend/routes.py
+++ backend/routes.py
@@ -1,7 +1,7 @@
 import json
 from flask import Flask, request
 from flasgger import Swagger
-from db import db, Post, Comment
+from db import db, Table, Club, User, Bid
 
 db_filename = 'posts.db'
 app = Flask(__name__)
@@ -32,88 +32,72 @@ PORT = 8000
 
 #### Code Begins Here #### 
 
-@app.route('/api/posts/')
-def get_posts():
+@app.route('/api/clubs')
+def get_clubs():
     '''
     file: ./documentation/get_posts.yml
     '''
 
-    posts = Post.query.all()
-    res = {'success': True, 'data': [post.serialize() for post in posts]}
+    clubs = Club.query.all()
+    res = {'success': True, 'data': [club.serialize() for club in clubs]}
     return json.dumps(res), 200
 
-@app.route('/api/posts/', methods = ['POST'])
-def create_post():
+@app.route('/api/club/<int:club_id>'
+def get_club(club_id):
     '''
     file: ./documentation/create_post.yml
     '''
-    
-    request_body = json.loads(request.data)
-    # Code here checks for blank body requests / @beforerequests checks for None body requests
-    if not request_body.get('text') == '' and not request_body.get('username') == '':
-        post = Post(
-            text = request_body.get('text'),
-            username = request_body.get('username')
-        )
-        # Keep the two acts separate to reduce automatic commits to the server
-        db.session.add(post)
-        db.session.commit()
-        return json.dumps({'success': True, 'data': post.serialize()}), 201
-    return json.dumps({'success': False, 'error': 'invalid body format'}), 412
 
-@app.route('/api/post/<int:post_id>/')
-def get_post(post_id):
+    club = Club.query.filter_by(id = club_id).first()
+    if club is not None:
+        return json.dumps({'success': True, 'data': post.serialize()}), 200
+    return json.dumps({'success': False, 'error': 'Post not found!'}), 404
+    
+
+@app.route('/api/club/<int:club_id>/tables')
+def get_club_tables(club_id):
     '''
     file: ./documentation/get_post.yml
     '''
 
-    post = Post.query.filter_by(id = post_id).first()
-    if post is not None:
-        return json.dumps({'success': True, 'data': post.serialize()}), 200
-    return json.dumps({'success': False, 'error': 'Post not found!'}), 404
+    club = Club.query.filter_by(id = club.id).first()
+    if club is not None:
+        tables = [table.serialize() for table in club.tables]
+        if len(tables) != 0:
+            return json.dumps({'success': True, 'data': comments}), 200
+        return json.dumps({'success': True, 'data': 'there are no tables in this club'}), 200
+    return json.dumps({'success': False, 'error': 'club not found'}), 404
 
-@app.route('/api/post/<int:post_id>/', methods = ['POST'])
-def update_post(post_id):
+
+@app.route('/api/club/<int:club_id>/table/<int:table_id>')
+def get_club_table(club_id, table_id):
     '''
     file: ./documentation/edit_post.yml
     '''
 
-    post = Post.query.filter_by(id = post_id).first()
-    if post is not None:
-        request_body = json.loads(request.data)
-        if not request_body.get('text') == '' and not request_body.get('username') == '':
-            post.text = request_body.get('text', post.text)
-            post.username = request_body.get('username', post.username)
-            db.session.commit()
-        return json.dumps({'success': True, 'data': post.serialize()}), 200
-    return json.dumps({'success': False, 'error': 'Post not found!'}), 404
+    club = Club.query.filter_by(id = club.id).first()
+    if club is not None:
+        table = club.Table.query.filter_by(id = table_id).first()
+            if table is not None:
+                return json.dumps({'success': True, 'data': table}), 200
+        return ({'success': False, 'error': 'table not found'}), 404
+    return ({'success': False, 'error': 'club not found'}), 404
+    
 
-@app.route('/api/post/<int:post_id>/', methods = ['DELETE'])
-def delete_post(post_id):
+@app.route('/api/table/<int:table_id>/bids')
+def get_table_bids(table_id):
     '''
     file: ./documentation/delete_post.yml
     '''
 
-    post = Post.query.filter_by(id = post_id).first()
-    if post is not None:
-        db.session.delete(post)
-        db.session.commit()
-        return json.dumps({'success': True, 'data': post.serialize()}), 200
-    return json.dumps({'success': False, 'error': 'Post not found!'}), 404
+    table = Table.query.filter_by(id = table_id).first()
+    if table is not None:
+        bids = [bid.serialize() for bid in table.bids]
+        if len(bids) != 0:
+            return json.dumps({'success': True, 'data': bids}), 200
+        return ({'success': True, 'data': 'there are no bids for this table'}), 200
+    return ({'success': False, 'error': 'club not found'}), 404
 
-@app.route('/api/post/<int:post_id>/comments/')
-def get_comments(post_id):
-    '''
-    file: ./documentation/get_comments.yml
-    '''
-
-    post = Post.query.filter_by(id = post_id).first()
-    if post is not None:
-        comments = [comment.serialize() for comment in post.comments]
-        if len(comments) != 0:
-            return json.dumps({'success': True, 'data': comments}), 200
-        return json.dumps({'success': True, 'data': 'There are no comments for this post.'}), 200
-    return json.dumps({'success': False, 'error': 'Post not found!'}), 404
 
 @app.route('/api/post/<int:post_id>/comment/', methods = ['POST'])
 def create_comment(post_id):
